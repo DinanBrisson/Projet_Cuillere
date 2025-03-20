e60a682df1a8cf3f8a025796edd8b3547286c590
Maxcym <136334069+Maxcym@users.noreply.github.com>
2025-03-20 20:22:33 +0100

    Add comments

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,6 @@
 from App.app import app, db
 
+# Initialisation de toutes les tables définies par les modèles SQLAlchemy dans la base de données
 with app.app_context():
     db.create_all()
     print("Base de données initialisée.")
